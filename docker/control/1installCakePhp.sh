3c3b71a1935003326cc5bbe5e849be9314bccc19
Sacha-Lewis Dmytruk <sacha@undologic.com>
2020-11-14 00:44:14 -0500

    upgrading...

--- docker/control/1installCakePhp.sh
+++ docker/control/1installCakePhp.sh
@@ -5,3 +5,19 @@ mv 2.x src
 
 #fix the permissions
 chmod -R 777 src/app/tmp/
+
+# Add layout from purchased template
+
+# add the display code
+# echo $this->Flash->render(); ?>
+# echo $this->fetch('content'); ?>
+
+# update the baseLayout links
+sed -i 's/href=\"c/href=\"<?= $baseLayout; ?>c/g' src/app/View/Layouts/default.ctp
+sed -i 's/href=\"i/href=\"<?= $baseLayout; ?>i/g' src/app/View/Layouts/default.ctp
+sed -i 's/src=\"i/src=\"<?= $baseLayout; ?>i/g' src/app/View/Layouts/default.ctp
+sed -i 's/src=\"j/src=\"<?= $baseLayout; ?>j/g' src/app/View/Layouts/default.ctp
+sed -i 's/img="i/img=\"<?= $baseLayout; ?>i/g' src/app/View/Layouts/default.ctp
+
+# ensure the baseLayout exists in the app_controller
+# $this->set('baseLayout', $this->webroot.'modules/site/'; ?>
