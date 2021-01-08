dc859c443566bfc97ef4326fa50aa9f02741d383
Sacha-Lewis Dmytruk <sacha@undologic.com>
2021-01-08 08:45:36 -0500

    upgraded launch proceedure to use git clone with deploy keys

--- launch/1_setupTestingServer.sh
+++ launch/1_setupTestingServer.sh
@@ -1,4 +1,13 @@
-# Variables
+# Setup
+# Requires deploy keys setup on github
+# get the public key from the live server (ssh into the server)
+# cd ~/.ssh
+# cat id_rsa.pub
+# copy that public key
+# Github.com -> Settings -> Deploy keys
+# Add Deploy Key
+# Add comment which server (so you remember later)
+# Paste in the key into the box
 SSH_HOST=$(grep '^ *"SSH_HOST":' settings.json | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 SSH_USER=$(grep '^ *"SSH_USER":' settings.json | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 GITHUB_CURRENT_BRANCH=$(grep '^ *"GITHUB_CURRENT_BRANCH":' settings.json | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
@@ -28,6 +37,9 @@ read -p "Press enter to continue"
 #ssh $SSH_USER@$SSH_HOST "cd $TESTING_ABSOLUTE_PATH && rm -rf $GITHUB_CURRENT_BRANCH && svn export --force --no-auth-cache --username $GITHUB_USERNAME --password $GITHUB_PASSWORD https://@github.com/$GITHUB_USER_SLASH_PROJECT/$GITHUB_CURRENT_BRANCH $TESTING_ABSOLUTE_PATH/$GITHUB_CURRENT_BRANCH" && echo ""
 ssh $SSH_USER@$SSH_HOST "cd $TESTING_ABSOLUTE_PATH && rm -rf $GITHUB_CURRENT_BRANCH && git clone git@github.com:$GITHUB_USER_SLASH_PROJECT.git --branch $GITHUB_CURRENT_BRANCH --single-branch $TESTING_ABSOLUTE_PATH/$GITHUB_CURRENT_BRANCH" && echo ""
 
+
 #open firefox new tab with link
 #figure out how to pass spaces from the settings page to here as the space is ending the variable
 "C:\Program Files\Firefox Developer Edition\firefox.exe" -new-tab $TESTING_URL/$GITHUB_CURRENT_BRANCH/$SRC_FILES_RELATIVE_PATH/
+
+read -p "Complete - Press enter to continue"
--- launch/3_go_LIVE.sh
+++ launch/3_go_LIVE.sh
@@ -1,10 +1,8 @@
 # Variables
 SSH_HOST=$(grep '^ *"SSH_HOST":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 SSH_USER=$(grep '^ *"SSH_USER":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
-GITHUB_CURRENT_BRANCH=$(grep '^ *"GITHUB_CURRENT_BRANCH":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
-GITHUB_USERNAME=$(grep '^ *"GITHUB_USERNAME":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
-GITHUB_PASSWORD=$(grep '^ *"GITHUB_PASSWORD":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 GITHUB_USER_SLASH_PROJECT=$(grep '^ *"GITHUB_USER_SLASH_PROJECT":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
+SERVER_TMP_LOCATION_ABSOLUTE_PATH=$(grep '^ *"SERVER_TMP_LOCATION_ABSOLUTE_PATH":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 SRC_FILES_RELATIVE_PATH=$(grep '^ *"SRC_FILES_RELATIVE_PATH":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 TESTING_URL=$(grep '^ *"TESTING_URL":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 TESTING_ABSOLUTE_PATH=$(grep '^ *"TESTING_ABSOLUTE_PATH":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
@@ -13,8 +11,9 @@ LIVE_ABSOLUTE_PATH=$(grep '^ *"LIVE_ABSOLUTE_PATH":' settings.json  | awk '{ pri
 BROWSER_LOCAL_PATH_WITH_PROGRAM=$(grep '^ *"BROWSER_LOCAL_PATH_WITH_PROGRAM":' settings.json  | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
 #
 #
-#
-#Rsync the files from test location to LIVE
+# Rsync the files from test location to LIVE
 ssh $SSH_USER@$SSH_HOST "rsync -av --omit-dir-times --no-perms $TESTING_ABSOLUTE_PATH/$GITHUB_CURRENT_BRANCH/$SRC_FILES_RELATIVE_PATH/. $LIVE_ABSOLUTE_PATH/." && echo ""
 #open firefox new tab with link
 "C:\Program Files\Firefox Developer Edition\firefox.exe" -new-tab $LIVE_URL
+
+read -p "Press enter to continue"
